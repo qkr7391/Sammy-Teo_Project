faeca1b88f64221ff3ae96a59e0aba5ff340adab
Sammy Park <saemipark7391@gmail.com>
2023-08-21 00:28:54 -0400

    install react for new version - Sammy

---
+++ new_version/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom"
+
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
+
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="header_nav_left">
+        <ul className="header_menu_left">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="header_nav_right">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+
+  <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="/login" element={<Login />} />
+     <Route path="/register" element={<Register />} />
+  </Routes>
+    </div>
+  );
+}
+
+export default App;

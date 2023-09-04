d43ca1658712c46f240cf79ddbe6dd2db431e441
Saemi Park <saemipark7391@gmail.com>
2023-09-04 17:27:57 -0400

    frontend css update

--- new_version/frontend/src/views/Register.js
+++ new_version/frontend/src/views/Register.js
@@ -2,68 +2,121 @@ import React, { useState } from "react";
 import axiosInstance from "../utils/axios";
 
 const Register = () => {
-    const [inputs, setInputs] = useState({
-        id: "",
-        pw: "",
-        pwCheck: "",
-        email: "",
-    });
+	const [inputs, setInputs] = useState({
+		id: "",
+		pw: "",
+		pwCheck: "",
+		email: "",
+	});
 
-    const { id, pw, pwCheck, email } = inputs;
+	const { id, pw, pwCheck, email } = inputs;
 
-    const onChange = (event) => {
-        const value = event.target.value;
-        const id = event.target.id;
+	const onChange = (event) => {
+		const value = event.target.value;
+		const id = event.target.id;
+		setInputs({
+			...inputs,
+			[id]: value,
+		});
+	};
 
-        setInputs({
-            ...inputs,
-            [id]: value,
-        });
-    };
+	const checkPW = (pw, pwCheck) => {
+		if (pw === pwCheck) {
+			return true;
+		}
+		return false;
+	};
 
-    const fetchData = async (event) => {
-        // await axios.get('url').then((response) => setInputs(response.data));
+	const fetchData = async (event) => {
+		// await axios.get('url').then((response) => setInputs(response.data));
 
-        try {
-            const response = await axiosInstance.post("/users/register", inputs);
-            console.log("Registration successful!", response.data);
-            // Handle success, redirect, or show a success message to the user
-        } catch (error) {
-            console.error("Registration failed:", error);
-            // Handle error, display error message, etc.
-        }
-    };
+		try {
+			const response = await axiosInstance.post("/users/register", inputs);
+			console.log("Registration successful!", response.data);
+			// Handle success, redirect, or show a success message to the user
+		} catch (error) {
+			console.error("Registration failed:", error);
+			// Handle error, display error message, etc.
+		}
+	};
 
-    return (
-        <>
-            <br></br>
-            <form onSubmit={fetchData}>
-                <label>ID </label>
-                <input type="text" id="id" value={id} onChange={onChange} />
-                <br></br>
-                <br></br>
+	return (
+		<>
+			<section>
+				<div>
+					<h1 className="text-3xl font-semibold text-center"> Register </h1>
+				</div>
 
-                <label>PASSWORD </label>
-                <input type="password" id="pw" value={pw} onChange={onChange} />
-                <br></br>
-                <br></br>
+				<form className="mt-6">
+					<div className="mb-2">
+						<label htmlFor="id" className="text-sm font-semibold text-grey-800">
+							{" "}
+							ID{" "}
+						</label>
+						<input
+							type="text"
+							id="id"
+							className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+							value={id}
+							onChange={onChange}
+						/>
+					</div>
 
-                <label>PASSWORD CONFIRM </label>
-                <input type="password" id="pwCheck" value={pwCheck} onChange={onChange} />
-                <br></br>
-                <br></br>
+					<div className="mb-2">
+						<label htmlFor="pw" className="text-sm font-semibold text-grey-800">
+							{" "}
+							PassWord{" "}
+						</label>
+						<input
+							type="password"
+							id="pw"
+							className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+							value={pw}
+							onChange={onChange}
+						/>
+					</div>
 
-                <label>EMAIL </label>
-                <input type="email" id="email" value={email} onChange={onChange} />
-                <br></br>
-                <br></br>
-            </form>
-            <button className="btn-primary" onClick={fetchData}>
-                {" "}
-                Register
-            </button>
-        </>
-    );
+					<div className="mb-2">
+						<label
+							htmlFor="pwCheck"
+							className="text-sm font-semibold text-grey-800"
+						>
+							{" "}
+							PassWord Confirm
+						</label>
+						<input
+							type="password"
+							id="pwCheck"
+							className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+							value={pwCheck}
+							onChange={onChange}
+						/>
+					</div>
+
+					<div className="mb-2">
+						<label
+							htmlFor="email"
+							className="text-sm font-semibold text-grey-800"
+						>
+							Email{" "}
+						</label>
+						<input
+							type="email"
+							id="email"
+							className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+							value={email}
+							onChange={onChange}
+						/>
+					</div>
+				</form>
+				<div className="mt-6">
+					<button className="btn-primary" onClick={fetchData}>
+						Register
+					</button>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Register;

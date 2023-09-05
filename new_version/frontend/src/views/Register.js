540f5459540bae3aeef4eac5d6c7fd87a713c2f0
Saemi Park <saemipark7391@gmail.com>
2023-09-05 10:47:27 -0400

    css for register form center

--- new_version/frontend/src/views/Register.js
+++ new_version/frontend/src/views/Register.js
@@ -46,75 +46,76 @@ const Register = () => {
 				<div>
 					<h1 className="text-3xl font-semibold text-center"> Register </h1>
 				</div>
+				<div className="flex justify-center ">
+					<form className="mt-6">
+						<div className="relative mb-2">
+							<label
+								htmlFor="id"
+								className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+							>
+								{" "}
+								ID{" "}
+							</label>
+							<input
+								type="text"
+								id="id"
+								className="w-12/12 px-4 py-2 mt-2 bg-white border rounded-md"
+								value={id}
+								onChange={onChange}
+							/>
+						</div>
 
-				<form className="mt-6">
-					<div className="relative mb-2">
-						<label
-							htmlFor="id"
-							className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-						>
-							{" "}
-							ID{" "}
-						</label>
-						<input
-							type="text"
-							id="id"
-							className="w-6/12 px-4 py-2 mt-2 bg-white border rounded-md"
-							value={id}
-							onChange={onChange}
-						/>
-					</div>
+						<div className="relative mb-2">
+							<label
+								htmlFor="pw"
+								className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+							>
+								{" "}
+								PassWord{" "}
+							</label>
+							<input
+								type="password"
+								id="pw"
+								className="w-12/12 px-4 py-2 mt-2 bg-white border rounded-md"
+								value={pw}
+								onChange={onChange}
+							/>
+						</div>
 
-					<div className="relative mb-2">
-						<label
-							htmlFor="pw"
-							className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-						>
-							{" "}
-							PassWord{" "}
-						</label>
-						<input
-							type="password"
-							id="pw"
-							className="w-6/12 px-4 py-2 mt-2 bg-white border rounded-md"
-							value={pw}
-							onChange={onChange}
-						/>
-					</div>
+						<div className="relative mb-2">
+							<label
+								htmlFor="pwCheck"
+								className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+							>
+								{" "}
+								PassWord Confirm
+							</label>
+							<input
+								type="password"
+								id="pwCheck"
+								className="w-12/12 px-4 py-2 mt-2 bg-white border rounded-md"
+								value={pwCheck}
+								onChange={onChange}
+							/>
+						</div>
 
-					<div className="relative mb-2">
-						<label
-							htmlFor="pwCheck"
-							className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-						>
-							{" "}
-							PassWord Confirm
-						</label>
-						<input
-							type="password"
-							id="pwCheck"
-							className="w-6/12 px-4 py-2 mt-2 bg-white border rounded-md"
-							value={pwCheck}
-							onChange={onChange}
-						/>
-					</div>
-
-					<div className="relative mb-2">
-						<label
-							htmlFor="email"
-							className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-						>
-							Email{" "}
-						</label>
-						<input
-							type="email"
-							id="email"
-							className="w-6/12 px-4 py-2 mt-2 bg-white border rounded-md"
-							value={email}
-							onChange={onChange}
-						/>
-					</div>
-				</form>
+						<div className="relative mb-2">
+							<label
+								htmlFor="email"
+								className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+							>
+								Email{" "}
+							</label>
+							<input
+								type="email"
+								id="email"
+								className="w-12/12 px-4 py-2 mt-2 bg-white border rounded-md"
+								value={email}
+								onChange={onChange}
+							/>
+						</div>
+					</form>
+				</div>
 				<div className="mt-6">
 					<button
 						className="border bg-blue-100 rounded-md px-4 py-2"

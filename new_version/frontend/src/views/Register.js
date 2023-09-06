00fccdd1c03ae8afa9b6907ada828fa9dbc26c9b
Saemi Park <saemipark7391@gmail.com>
2023-09-06 13:37:04 -0400

    make another object for sending data

--- new_version/frontend/src/views/Register.js
+++ new_version/frontend/src/views/Register.js
@@ -4,12 +4,12 @@ import axiosInstance from "../utils/axios";
 const Register = () => {
 	const [inputs, setInputs] = useState({
 		name: "",
-		pw: "",
+		password: "",
 		pwCheck: "",
 		email: "",
 	});
 
-	const { name, pw, pwCheck, email } = inputs;
+	const { name, password, pwCheck, email } = inputs;
 
 	const onChange = (event) => {
 		const value = event.target.value;
@@ -20,8 +20,8 @@ const Register = () => {
 		});
 	};
 
-	const checkPW = (pw, pwCheck) => {
-		if (pw === pwCheck) {
+	const checkPW = (password, pwCheck) => {
+		if (password === pwCheck) {
 			return true;
 		}
 		return false;
@@ -29,14 +29,14 @@ const Register = () => {
 
 	const fetchData = async (event) => {
 		// await axios.get('url').then((response) => setInputs(response.data));
-
+		const data = { name, password, email };
 		//Distinguish that password and passwordcheck is same or not
-		if (!checkPW(pw, pwCheck)) {
+		if (!checkPW(password, pwCheck)) {
 			alert("Password do not match");
 			console.log("Registration failed: check Password and Password Confirm");
 		} else {
 			try {
-				const response = await axiosInstance.post("/users/register", inputs);
+				const response = await axiosInstance.post("/users/register", data);
 				console.log("Registration successful!", response.data);
 				// Handle success, redirect, or show a success message to the user
 			} catch (error) {
@@ -73,7 +73,7 @@ const Register = () => {
 
 						<div className="relative mb-4">
 							<label
-								htmlFor="pw"
+								htmlFor="password"
 								className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
 							>
 								{" "}
@@ -81,9 +81,9 @@ const Register = () => {
 							</label>
 							<input
 								type="password"
-								id="pw"
+								id="password"
 								className="w-80 px-4 py-2 mt-2 bg-white border rounded-md"
-								value={pw}
+								value={password}
 								onChange={onChange}
 							/>
 						</div>
